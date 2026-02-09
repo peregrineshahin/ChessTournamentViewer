31226d8a7baa414f822ee8ef8be05412d94d5baa
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-02-09 19:09:09 +0100

    Improve stockfish analysis

--- src/components/StockfishWorker.ts
+++ src/components/StockfishWorker.ts
@@ -8,6 +8,7 @@ export class StockfishWorker {
     onMessage: ((liveInfo: CCCLiveInfo, arrow: DrawShape | null) => void) | null = null
     currentFen: string | null = null
     receivedBestMove: boolean = true
+    stopping: boolean = false
 
     constructor() {
         this.worker = new Worker("/stockfish-17.1-single-a496a04.js")
@@ -16,13 +17,16 @@ export class StockfishWorker {
             if (e.data.includes("bestmove"))
                 this.receivedBestMove = true
 
-            if (!e.data.startsWith("info depth")) return;
+            if (this.stopping || !e.data.startsWith("info depth")) return;
             if (e.data.includes("currmove")) return;
             if (!this.currentFen || !this.onMessage || this.currentFen === "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") return;
             
             const data = e.data.split(" ")
             const color = this.currentFen.includes(" w ") ? "white" : "black"
-            const ply = 2 * Number(this.currentFen.split(" ").at(-1)) - (color === "white" ? 1 : 0)
+            const ply = 2 * Number(this.currentFen.split(" ").at(-1)) - (color === "white" ? 1 : 0) - 1
+
+            const time = Number(data[17])
+            if (isNaN(time) || time <= 100) return
 
             const liveInfo: CCCLiveInfo = {
                 type: "liveInfo",
@@ -35,7 +39,7 @@ export class StockfishWorker {
                     name: "",
                     nodes: data[11],
                     pv: "",
-                    score: String(Number(data[9]) / 100),
+                    score: String(Number(data[9]) / 100 * (color === "black" ? -1 : 1)),
                     seldepth: data[4],
                     speed: data[13],
                     tbhits: "",
@@ -48,6 +52,9 @@ export class StockfishWorker {
             this.onMessage(liveInfo, arrow)
         }
         this.sendMessage("uci")
+        this.sendMessage("setoption name Hash value 128")
+        this.sendMessage("isready")
+        this.sendMessage("ucinewgame")
     }
 
     sendMessage(command: string) {
@@ -56,7 +63,7 @@ export class StockfishWorker {
 
     async analyze(fen: string) {
         if (this.currentFen === fen) return
-        this.currentFen = fen
+        this.stopping = true
 
         this.sendMessage("stop")
         const waitForReady = () => {
@@ -70,6 +77,8 @@ export class StockfishWorker {
         };
         await waitForReady()
 
+        this.currentFen = fen
+        this.stopping = false
         this.receivedBestMove = false
         this.sendMessage(`position fen ${fen}`)
         this.sendMessage("go infinite")
